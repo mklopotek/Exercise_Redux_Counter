d7283d6128cdabb4f3d3b96df2d2a9766c034d0b
Magda Kłopotek <magda.klopotek@gmail.com>
2018-08-13 14:58:38 +0200

    reset button

--- src/Counter/CounterButtons.js
+++ src/Counter/CounterButtons.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux'
 
-import { add, sub } from './store'
+import { add, sub, reset } from './store'
 
-const mapDispatchToProps= (dispatch) => ({
-    isaAdd: () => dispatch(add()),
-    isaSub: () => dispatch(sub())
-}) 
+const mapDispatchToProps = (dispatch) => ({
+    isaAdd: (number) => dispatch(add(number)),
+    isaSub: (number) => dispatch(sub(number)),
+    isaReset: () => dispatch(reset())
+})
 
 class CounterButtons extends React.Component {
+    state = {
+        inputValue: 0
+    }
 
+    handlerChange = (event) => {
+        this.setState({ inputValue: parseInt(event.target.value ? event.target.value : 0) })
+    }
 
     render() {
         return (
             <div>
+                <input
+                    type='number'
+                    placeholder='number'
+                    value={this.state.inputValue}
+                    onChange={this.handlerChange}
+                />
                 <button
-                    onClick={this.props.isaAdd}
+                    onClick={() => this.props.isaAdd(this.state.inputValue)}
                 >
                     Add
                 </button>
                 <button
-                    onClick={this.props.isaSub}
+                    onClick={() => this.props.isaSub(this.state.inputValue)}
                 >
                     Sub
+                </button>
+                <button
+                    onClick={() => this.props.isaReset()}
+                >
+                    Reset
                 </button>
             </div>
 
--- src/Counter/store.js
+++ src/Counter/store.js
@@ -1,15 +1,25 @@
 const ADD = 'ADD'
 const SUB = 'SUB'
+const RESET = 'RESET'
 
-export const add = () => (
+export const add = (number = 1) => (
     {
-        type: ADD
+        type: ADD,
+        number
+        //the same like number: number
     }
 )
 
-export const sub = () => (
+export const sub = (number = 1) => (
     {
-        type: SUB
+        type: SUB,
+        number
+    }
+)
+
+export const reset = () => (
+    {
+        type: RESET,
     }
 )
 
@@ -22,12 +32,17 @@ const reducer = (state = INITIAL_STATE, action) => {
         case ADD:
             return {
                 ...state,
-                counterValue: state.counterValue + 1
+                counterValue: state.counterValue + action.number
             };
         case SUB:
         return {
             ...state,
-            counterValue: state.counterValue - 1
+            counterValue: state.counterValue - action.number
+        };
+        case RESET:
+        return {
+            ...state,
+            counterValue: 0
         };
         default:
             return state;
